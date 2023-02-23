f61513df84dd768dbb02f4f455e42a391e1fe267
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-23 21:38:55 +0900

    list_task

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,5 +1,7 @@
+use chrono::Local;
 use chrono::{serde::ts_seconds, DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use std::fmt;
 use std::fs::{File, OpenOptions};
 use std::io::{Error, ErrorKind, Result, Seek};
 use std::path::PathBuf;
@@ -16,6 +18,13 @@ impl Task {
     pub fn new(text: String) -> Task {
         let created_at: DateTime<Utc> = Utc::now();
         Task { text, created_at }
+    }
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
     }
 }
 
@@ -48,6 +57,23 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> 
 
     file.set_len(0)?;
     serde_json::to_writer(file, &tasks)?;
+    Ok(())
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    let tasks = collect_tasks(&file)?;
+
+    if tasks.is_empty() {
+        print!("Task list is empty!");
+    } else {
+        let mut order: u32 = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+            order += 1;
+        }
+    }
+
     Ok(())
 }
 

2e88d11b29277b936637ccb279bbe02939ce509a
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-23 21:16:24 +0900

    add_task, complete_task

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,8 +1,14 @@
-use chrono::{DateTime, Utc};
+use chrono::{serde::ts_seconds, DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::fs::{File, OpenOptions};
+use std::io::{Error, ErrorKind, Result, Seek};
+use std::path::PathBuf;
 
-// #[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
+
+    #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
 
@@ -11,4 +17,47 @@ impl Task {
         let created_at: DateTime<Utc> = Utc::now();
         Task { text, created_at }
     }
+}
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+
+    let mut tasks = collect_tasks(&file)?;
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
+}
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+
+    let mut tasks = collect_tasks(&file)?;
+
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+    }
+    tasks.remove(task_position - 1);
+
+    file.set_len(0)?;
+    serde_json::to_writer(file, &tasks)?;
+    Ok(())
+}
+
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.rewind();
+    let tasks: Vec<Task> = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+    file.rewind();
+    Ok(tasks)
 }
